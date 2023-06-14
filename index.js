5ee28f464491fc5a95136be0db125307248d1762
Yeakub1 <mryeakub10@gmail.com>
2023-06-14 22:25:51 +0600

    delete add

--- index.js
+++ index.js
@@ -141,6 +141,7 @@ async function run() {
       res.send(result);
     });
 
+    // delete
     app.delete("/class/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
